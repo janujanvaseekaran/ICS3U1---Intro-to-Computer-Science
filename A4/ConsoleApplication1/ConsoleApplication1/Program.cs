49768e3ae83d3471ca4dcc20433d6d3c7d8326fe
jking-rules2007 <164825295+jking-rules2007@users.noreply.github.com>
2024-03-24 22:37:49 -0400

    A4 Assignment

---
+++ A4/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿//Janujan Vaseekaran
+// ICS3U1 - Into to Computer Science 
+// A4 Assignment 
+// 2024-02-17
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("12 x 1 = " + (12 * 1)); //Displays " 12 x 1 = " on the command prompt as well as displaying the product of the two integers as an equation 
+            Console.WriteLine("12 x 2 = " + (12 * 2)); //Displays " 12 x 2 = " on the command promptas well as displaying the product of the two integers as an equation
+            Console.WriteLine("12 x 3 = " + (12 * 3)); //Displays " 12 x 3 = " on the command prompt as well as displaying the product of the two integers as an equation
+            Console.WriteLine("12 x 4 = " + (12 * 4));//Displays " 12 x 4 = " on the command promptas as well as displaying the product of the two integers as an equation
+            Console.WriteLine("12 x 5 = " + (12 * 5)); //Displays " 12 x 5 = " on the command prompt as well as displaying the product of the two integers as an equation
+            Console.WriteLine("12 x 6 = " + (12 * 6)); //Displays " 12 x 6 = " on the command prompt as well as displaying the product of the two integers as an equation
+            Console.WriteLine("12 x 7 = " + (12 * 7)); //Displays " 12 x 7 = " on the command prompt as well as displaying the product of the two integers as an equation
+            Console.WriteLine("12 x 8 = " + (12 * 8)); //Displays " 12 x 8 = " on the command prompt as well as displaying the product of the two integers as an equation
+            Console.WriteLine("12 x 9 = " + (12 * 9)); //Displays " 12 x 9 = " on the command prompt as well as displaying the product of the two integers as an equation
+            Console.WriteLine("12 x 10 = " + (12 * 10)); //Displays " 12 x 10 = " on the command prompt as well as displaying the product of the two integers as an equation
+            Console.WriteLine("12 x 11 = " + (12 * 11)); //Displays " 12 x 11 = " on the command prompt as well as displaying the product of the two integers as an equation
+            Console.WriteLine("12 x 12 = " + (12 * 12)); //Displays " 12 x 12 = " on the command prompt as well as displaying the product of the two integers as an equation
+
+            Console.ReadKey(); //prevent the Console from closing untill you press enter
+        }
+    }
+}
