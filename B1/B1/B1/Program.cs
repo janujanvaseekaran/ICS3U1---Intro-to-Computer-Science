b7c0081de38558e4b65472860aaebbe3bdb54e6f
jking-rules2007 <164825295+jking-rules2007@users.noreply.github.com>
2024-03-24 22:54:08 -0400

    All Code Update

---
+++ B1/B1/B1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.Write("2 x ");
+                Console.WriteLine(i + " = " + i *2);
+               
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
