ac229d28f34ed4850b3f55f6d928aab3616c4290
jking-rules2007 <164825295+jking-rules2007@users.noreply.github.com>
2024-03-24 23:03:13 -0400

    All Code Update

---
+++ B7/B7/B7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+
+            while (num != 0)
+            {
+                Console.WriteLine("Enter Number: ");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.ReadKey();
+        }
+    }
+}
