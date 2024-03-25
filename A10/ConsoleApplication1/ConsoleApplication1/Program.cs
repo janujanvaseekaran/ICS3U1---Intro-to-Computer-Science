e93e92e92d944ceea9130d8b9a7756b275765252
jking-rules2007 <164825295+jking-rules2007@users.noreply.github.com>
2024-03-24 22:53:09 -0400

    All Code Update

---
+++ A10/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿//Janujan Vaseekaran
+// ICS3U1 - Into to Computer Science 
+// A10 Assignment 
+// 2024-02-17
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = ""; //declares a string variable called firstName 
+            Console.Write("Please enter your first name: "); //prompts the user to enter their first name 
+            firstName = Console.ReadLine(); //gets the first name entered from the user
+            string lastName = ""; //declares a string variable called lastName 
+            Console.Write("Please enter your last name: "); //prompts the user to enter their last name 
+            lastName = Console.ReadLine(); //gets the last name entered from the user 
+            int grade = 0; //declares a grade variable and assigns a default value 
+            Console.Write("Enter an your Grade: "); //prompts the user to enter their Grade
+            grade = Convert.ToInt32(Console.ReadLine()); //gets the grade number from the user
+            Console.WriteLine("Hello " + firstName + " " + lastName + " How are you doing today? I hope your doing fine in grade " + grade + " enjoy this year"); //prints the first and last name in a friendly greeting which includes the grade of the student
+            Console.ReadKey(); //Prevents the console from closing untill you press enter
+        }
+    }
+}
