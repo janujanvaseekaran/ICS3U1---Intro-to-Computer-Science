4cda0206845790edfa2caec7958e6090dfb39e05
jking-rules2007 <164825295+jking-rules2007@users.noreply.github.com>
2024-03-24 22:47:54 -0400

    All Code Update

---
+++ A6/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName; //declares a string variable called firstName
+            string lastName; //declares a string variable called lastName 
+            firstName = "Janujan"; //String variable called firstName is given the value Janujan
+            lastName = "Vaseekaran"; //String variable called lastName is given the value Vaseekaran
+            int grades = 11; //declares a variable called grades and gives a value of 11
+            Console.WriteLine("Hello " + firstName + " " + lastName + " you are in grade: " + grades); //Here the variables firstName, lastName, and grades are used to print a message in the command prompt
+            Console.ReadKey();//Prevents the console from closing until you press enter
+
+        }
+    }
+}
